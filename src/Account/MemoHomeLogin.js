bc7a7c727c94f0e8f5550b66198e6044e4aa4237
judicaellenallet@gmail.com <@Miette123>
2020-11-28 18:24:22 +0100

    authent

--- src/Account/MemoHomeLogin.js
+++ src/Account/MemoHomeLogin.js
@@ -27,7 +27,8 @@ const MemoHomeLogin = ({getTokenAuth}) => {
   
       
       const entry = {username: username, password:password}
-  
+      console.log("username : ",username)
+      console.log("password : ",password)
       fetch(url + "/authenticate", { 
         method: 'post', 
         headers: new Headers({
